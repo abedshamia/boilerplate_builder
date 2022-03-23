f8d65149df92efd2b1477c4b0e26f129aa1e71f9
abedshamia <ab.shamia96@gmail.com>
2022-03-23 20:26:34 +0200

    add helmet and cors middleware use

--- build.js
+++ build.js
@@ -230,8 +230,9 @@ function writeServerfile() {
 }
 
 function writeAppFile() {
-  const appFile = `const express = require('express');
+  const appFile = `
 const { join } = require('path');
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middlewares/errorHandler');
 const app = express();
@@ -240,6 +241,8 @@ app.disable('x-powered-by');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, '..', 'public')));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
 
 app.use(errorHandler);
